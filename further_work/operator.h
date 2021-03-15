2c22b87c8de055e5c14fd44cfb8a8fa83704a325
wangyuyue <wangyuyue99@gmail.com>
2021-03-15 20:07:30 +0800

    omit uncompleted work

--- further_work/operator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "dataflow.h"
-
-PEArray Load2DSquarePEArray(int height, int width);
-PEArray Load1DPEArray(int size);
-Statement LoadConv2d(int k, int c, int x, int y, int rx, int ry, int stride);
-Statement LoadGemm(int n, int c, int m);
-Dataflow LoadNlrConv2d(PEArray& pe_, Statement& st_);
